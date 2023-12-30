d3c11e8ee08393fd83519903955ed09bb38a0239
Suraj <surajpynb@gmail.com>
2023-12-30 11:23:09 +0530

    Update ajax-form.js

--- assets/js/ajax-form.js
+++ assets/js/ajax-form.js
@@ -1,24 +1,26 @@
 (function ($) {
     'use strict';
     var form = $('.contact-form'),
-        message = $('.messenger-box-contact__msg'),
+        message1 = $('.messenger-box-contact__msg'),
         form_data;
 
     const submit = document.getElementById("submit-form");
+    
     submit.addEventListener("click", validate);
+
     function validate(e) {
-        const message = document.getElementById('required-msg');
+        const message1 = document.getElementById('required-msg');
 
         const fullName = document.getElementById("full-name");
         const email = document.getElementById("email");
-        const subject = document.getElementById("subject");
+        const subject1 = document.getElementById("subject");
         let valid = true;
 
-        if (!fullName.value || !email.value || !subject.value) {
-            message.classList.add('show');
+        if (!fullName.value || !email.value || !subject1.value) {
+            message1.classList.add('show');
             fullName.classList.add("invalid");
         } else {
-            message.classList.remove('show');
+            message1.classList.remove('show');
         }
         
         return valid;
@@ -27,29 +29,29 @@
 
     // Success function
     function done_func(response) {
-        message.fadeIn().removeClass('alert-danger').addClass('alert-success');
-        message.text(response);
+        message1.fadeIn().removeClass('alert-danger').addClass('alert-success');
+        message1.text(response);
         setTimeout(function () {
-            message.fadeOut();
+            message1.fadeOut();
         }, 3000);
         form.find('input:not([type="submit"]), textarea').val('');
     }
 
     // fail function
     function fail_func(data) {
-        message.fadeIn().removeClass('alert-success').addClass('alert-success');
-        message.text(data.responseText);
+        message1.fadeIn().removeClass('alert-success').addClass('alert-success');
+        message1.text(data.responseText);
         setTimeout(function () {
-            message.fadeOut();
+            message1.fadeOut();
         }, 3000);
     }
 
-    function sendemail(fullname, email, subject, message) {
+    function sendemail(fullname, email, subject1, message1) {
         emailjs.send("service_znkp1eg", "template_683eaxa", {
             from_name: email,
             to_name: fullname,
-            subject: subject,
-            message: message,
+            subject: subject1,
+            message: message1
         });
     };
 
@@ -65,23 +67,23 @@
         e.preventDefault();
 
         
-        const message = document.getElementById('required-msg');
+        const message1 = document.getElementById('message');
 
         const fullName = document.getElementById("full-name");
         const email = document.getElementById("email");
-        const subject = document.getElementById("subject");
+        const subject1 = document.getElementById("subject");
 
         if (!fullName.value || !email.value) {
-            message.classList.add('show');
+            message1.classList.add('show');
             fullName.classList.add("invalid");
             console.log('false');
             return false
         }
         else {
-                sendemail(fullName.value, email.value, subject.value, message.value);
+                sendemail(fullName.value, email.value, subject1.value, message1.value);
                 // success();
             }
-        message.classList.remove('show');
+        message1.classList.remove('show');
 
         form_data = $(this).serialize();
         $.ajax({
